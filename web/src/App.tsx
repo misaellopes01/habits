c582a2483e4f86b65c3a452e37c7afcabc9a1bba
Misael Lopes <mecl.ely@gmail.com>
2023-08-24 14:16:37 +0100

    home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
-function App() {
-
+export function App() {
   return (
-    <div className='bg-slate-700'>
-     <p>Alalal</p>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
-
-export default App
