c83803630171412159bd18a3aa1f87a5c55bf523
Misael Lopes <mecl.ely@gmail.com>
2023-08-25 02:05:48 +0100

    backend: toggle completed and get summary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,13 @@
-import { FastifyInstance } from "fastify"
+import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import dayjs from 'dayjs'
-import { prisma } from "./lib/prisma"
+import { prisma } from './lib/prisma'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/habits', async (request, reply) => {
-
     const createHabitBody = z.object({
       title: z.string(),
-      weekDays: z.array(z.number().min(0).max(6))
+      weekDays: z.array(z.number().min(0).max(6)),
     })
     const { title, weekDays } = createHabitBody.parse(request.body)
     const today = dayjs().startOf('day').toDate()
@@ -17,21 +16,21 @@ export async function appRoutes(app: FastifyInstance) {
         title,
         created_at: today,
         weekDays: {
-          create: weekDays.map(weekDay => {
+          create: weekDays.map((weekDay) => {
             return {
-              week_day: weekDay
+              week_day: weekDay,
             }
-          })
-        }
-      }
+          }),
+        },
+      },
     })
 
     return reply.status(200).send()
   })
 
-  app.get('/day', async (request, reply) => {
+  app.get('/day', async (request) => {
     const getDayParams = z.object({
-      date: z.coerce.date()
+      date: z.coerce.date(),
     })
     const { date } = getDayParams.parse(request.query)
     const parsedDate = dayjs(date).startOf('day')
@@ -41,32 +40,111 @@ export async function appRoutes(app: FastifyInstance) {
     const possibleHabits = await prisma.habit.findMany({
       where: {
         created_at: {
-          lte: date
+          lte: date,
         },
         weekDays: {
           some: {
-            week_day: weekDay
-          }
-        }
-      }
+            week_day: weekDay,
+          },
+        },
+      },
     })
 
     const day = await prisma.day.findFirst({
       where: {
-        date: parsedDate.toDate()
-      }, 
+        date: parsedDate.toDate(),
+      },
       include: {
-        dayHabits: true
-      }
+        dayHabits: true,
+      },
     })
 
-    const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+    const completedHabits =
+      day?.dayHabits.map((dayHabit) => dayHabit.habit_id) ?? []
 
     return {
       possibleHabits,
-      completedHabits
+      completedHabits,
     }
   })
+
+  // set/unset as completed
+  // if I want to implement a feature that allow the user complete an habit in a
+  // specific day I must modify the code to work with this rule
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findFirst({
+      where: {
+        date: today,
+      },
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      })
+    }
+    // verifying if the habit is already completed in that day
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    })
+    // if yes, unset completed
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      })
+    } else {
+      // set completed
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      })
+    }
+  })
+
+  app.get('/summary', async () => {
+    // [{}, {}, {}]
+    // total of possible habits in that date and how much I've completed, and comparison between
+    const summary = await prisma.$queryRaw`
+    SELECT 
+      D.id, 
+      D.date,
+      (
+        SELECT 
+          cast(count(*) as float)
+        FROM day_habits DH
+        WHERE DH.day_id = D.id
+      ) as completed,
+      (
+        SELECT 
+          cast(count(*) as float)
+        FROM habit_week_days HWD
+        JOIN habits H
+          ON H.id = HWD.habit_id
+        WHERE 
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+      ) as amount
+    FROM days D
+    `
+
+    return summary
+  })
 }
-
-
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import { appRoutes } from "./routes";
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
 
 app.register(cors)
 app.register(appRoutes)
-app.listen({
-  port: 3333
-}).then(() => console.log('Things up and watching the server'))
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('Things up and watching the server'))
