277649cdd53d3fa90e4782cd679286a499141eaf
Misael Lopes <mecl.ely@gmail.com>
2023-08-30 20:55:34 +0100

    web: new habit form api integration

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react'
 import { generateRangeFromYearBeginning } from '../utils/generate-range-from-year-beginning'
 import { HabitDay } from './HabitDay'
+import { api } from '../lib/axios'
+import dayjs from 'dayjs'
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateRangeFromYearBeginning()
+
+const minimumSummaDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaDatesSize - summaryDates.length
+
+interface SummaryResponseProps {
+  id: string
+  date: string
+  completed: number
+  amount: number
+}
 
 export function SummaryTable() {
-  const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
-
-  const summaryDates = generateRangeFromYearBeginning()
-
-  const minimumSummaDatesSize = 18 * 7 // 18 weeks
-  const amountOfDaysToFill = minimumSummaDatesSize - summaryDates.length
-
+  const [summary, setSummary] = useState<SummaryResponseProps[]>([])
+  useEffect(() => {
+    api
+      .get<SummaryResponseProps[]>('/summary')
+      .then((response) => setSummary(response.data))
+  }, [])
   return (
     <main className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3 pb-4">
@@ -25,11 +41,15 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3 overflow-x-auto pb-2">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
           return (
             <HabitDay
               key={date.toDateString()}
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           )
         })}
