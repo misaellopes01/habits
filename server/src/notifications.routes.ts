daeb7aa6f74eb92d357da29867350a7ff511ee30
Misael Lopes <mecl.ely@gmail.com>
2023-09-04 01:31:09 +0100

    push notification on web

---
+++ server/src/notifications.routes.ts
@@ -0,0 +1,45 @@
+import WebPush from 'web-push'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+const keys = WebPush.generateVAPIDKeys()
+
+const publicKey =
+  'BJws0NDUYrNV0bxLsshRCnf8y3k0KuzPNd-eYI8MwkwEgBb4tarLgdMPktj1r_LGxWBnIQyCy2vIe8HDvt5KYbg'
+const privateKey = 'alQWdit3OaV5eYykMF7qqVqnYpym31arKDyCFrGL2Hw'
+
+WebPush.setVapidDetails('https://localhost:3333', publicKey, privateKey)
+
+export async function notificationsRoutes(app: FastifyInstance) {
+  app.get('/push/public_key', () => {
+    return {
+      publicKey,
+    }
+  })
+
+  app.post('/push/register', (request, reply) => {
+    console.log(request.body)
+
+    return reply.status(201).send()
+  })
+
+  app.post('/push/send', async (request, reply) => {
+    const sendPushBody = z.object({
+      subscription: z.object({
+        endpoint: z.string(),
+        keys: z.object({
+          p256dh: z.string(),
+          auth: z.string(),
+        }),
+      }),
+    })
+
+    const { subscription } = sendPushBody.parse(request.body)
+
+    setTimeout(() => {
+      WebPush.sendNotification(subscription, 'Hello do Backend')
+    }, 5000)
+
+    return reply.status(201).send()
+  })
+}
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,38 @@ import './lib/dayjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
+import { api } from './lib/axios'
+
+// window.Notification.requestPermission((permission) => {
+//   if (permission === 'granted') {
+//     // eslint-disable-next-line no-new
+//     new window.Notification('Habits', {
+//       body: 'Texto',
+//     })
+//   }
+// })
+navigator.serviceWorker
+  .register('service-worker.js')
+  .then(async (serviceWorker) => {
+    let subscription = await serviceWorker.pushManager.getSubscription()
+
+    if (!subscription) {
+      const notificationPublicKey = await api.get('/push/public_key')
+
+      subscription = await serviceWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: notificationPublicKey.data.publicKey,
+      })
+    }
+
+    await api.post('/push/register', {
+      subscription,
+    })
+
+    await api.post('/push/send', {
+      subscription,
+    })
+  })
 
 export function App() {
   return (
